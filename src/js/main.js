578b8474b530e49c0a4bb65f4bdc85ea99c9384c
Arek-Laptop <arekpiotrowicz@gmail.com>
2025-02-07 00:02:38 +0100

    Starting js. Function create new job

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,24 @@
 //Buttons
-const jobBtn = document.querySelector('job-button')
-const eduBtn = document.querySelector('edu-button')
-const langBtn = document.querySelector('language-button')
+const jobBtn = document.querySelector('.job-button')
+const eduBtn = document.querySelector('.edu-button')
+const langBtn = document.querySelector('.language-button')
 
+const createJob = () => {
+	const cvBoxJob = document.getElementsByClassName('cv-box-job')
+	const jobForm = cvBoxJob.createElement('form')
+	const label = jobForm.createElement('label')
+	const input = jobForm.createElement('input')
+	jobForm.setAttribute('action', 'atrybut')
+	jobForm.classList.add('job-form')
+	label.setAttribute('for', 'company')
+	label.textContent = 'Firma:'
+	input.setAttribute('type', 'text')
+	input.setAttribute('id', 'company')
+	input.setAttribute('name', 'company')
 
+	jobForm.append(label,input)
+	cvBoxJob.append(jobForm)
+   
+}
+// createJob()
+jobBtn.addEventListener('click', createJob)
