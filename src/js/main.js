02c507d54550395cbfd156ed9deef157f8582b13
Arek <arekpiotrowicz@gmail.com>
2025-02-15 20:48:44 +0100

    Added skill generation function and function to delete form

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@
 const jobBtn = document.querySelector('.job-button')
 const eduBtn = document.querySelector('.edu-button')
 const langBtn = document.querySelector('.language-button')
-
+const skillBtn = document.querySelector('.skills-button')
 
 // Funkcja generująca nowy formularz dla doświadczenia zawodowego
 const createJob = () => {
@@ -24,7 +24,6 @@ const createJob = () => {
 		const label = document.createElement('label')
 		label.setAttribute('for', field.id)
 		label.textContent = field.label
-		
 
 		let input
 		if (field.type === 'textarea') {
@@ -40,10 +39,14 @@ const createJob = () => {
 		input.setAttribute('name', field.name)
 		jobForm.append(label, input)
 	})
-	
+
 	const deleteBtn = document.createElement('button')
 	deleteBtn.classList.add('delete')
 	deleteBtn.textContent = 'Usuń'
+	deleteBtn.addEventListener('click', e => {
+		e.preventDefault()
+		jobForm.remove()
+	})
 	jobForm.append(deleteBtn)
 	cvBoxJob.append(jobForm)
 }
@@ -82,6 +85,10 @@ const createEdu = () => {
 	const deleteBtn = document.createElement('button')
 	deleteBtn.classList.add('delete')
 	deleteBtn.textContent = 'Usuń'
+	deleteBtn.addEventListener('click', e => {
+		e.preventDefault()
+		eduForm.remove()
+	})
 	eduForm.append(deleteBtn)
 	cvBoxEdu.append(eduForm)
 }
@@ -92,7 +99,7 @@ const createLanguage = () => {
 	langForm.setAttribute('action', '')
 	langForm.classList.add('language-form')
 	const fields = [
-		{ label: 'Umiejętności:', type: 'text', id: 'language', name: 'language', placeholder: 'np. Angielski' },
+		{ label: 'Język:', type: 'text', id: 'language', name: 'language', placeholder: 'np. Angielski' },
 		{ label: 'Poziom:', type: 'text', id: 'level', name: 'level', placeholder: 'np. C1' },
 	]
 
@@ -115,10 +122,51 @@ const createLanguage = () => {
 	const deleteBtn = document.createElement('button')
 	deleteBtn.classList.add('delete')
 	deleteBtn.textContent = 'Usuń'
+	deleteBtn.addEventListener('click', e => {
+		e.preventDefault() 
+		langForm.remove() 
+	})
 	langForm.append(deleteBtn)
 	cvBoxLang.append(langForm)
+}
+
+const createSkills = () => {
+	const cvBoxSkills = document.querySelector('.cv-box-skills')
+	const skillForm = document.createElement('form')
+	skillForm.setAttribute('action', '')
+	skillForm.classList.add('skills-form')
+
+	const fields = [
+		{ label: 'Umiejętności:', type: 'text', id: 'skills', name: 'skills', placeholder: 'HTML, CSS, JavaScript' },
+	]
+	fields.forEach(field => {
+		const label = document.createElement('label')
+		label.setAttribute('for', field.id)
+		label.textContent = field.label
+
+		let input
+		input = document.createElement('input')
+		input.setAttribute('type', field.type)
+		if (field.placeholder) {
+			input.setAttribute('placeholder', field.placeholder)
+		}
+
+		input.setAttribute('id', field.id)
+		input.setAttribute('name', field.name)
+		skillForm.append(label, input)
+	})
+	const deleteBtn = document.createElement('button')
+	deleteBtn.classList.add('delete')
+	deleteBtn.textContent = 'Usuń'
+	deleteBtn.addEventListener('click', e => {
+		e.preventDefault()
+		skillForm.remove()
+	})
+	skillForm.append(deleteBtn)
+	cvBoxSkills.append(skillForm)
 }
 
 jobBtn.addEventListener('click', createJob)
 eduBtn.addEventListener('click', createEdu)
 langBtn.addEventListener('click', createLanguage)
+skillBtn.addEventListener('click', createSkills)
