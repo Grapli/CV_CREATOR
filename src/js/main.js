981933e00697827bda73e53eef718f0a2903d431
Arek-Laptop <arekpiotrowicz@gmail.com>
2025-03-09 18:50:28 +0100

    clean code1

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-// //Buttons
 const jobBtn = document.querySelector('.job-button')
 const eduBtn = document.querySelector('.edu-button')
 const langBtn = document.querySelector('.language-button')
@@ -37,95 +36,6 @@ const createJob = (data = {}) => {
 	})
 	jobContainer.appendChild(form)
 }
-const createEduForm = (data = {}) => {
-	const eduContainer = document.querySelector('.cv-box-edu') // Kontener na formularze
-	const form = document.createElement('form')
-	form.classList.add('edu-form', 'form')
-	form.setAttribute('data-id', data.id || Date.now().toString())
-	form.innerHTML = `
-		<label for="school" class="label">Szkoła/Uczelnia </label>
-        <input type="text" class="input" name="school" placeholder="Nazwa szkoły" value="${data.school || ''}" />
-		<label for="degree" class="label">Kierunek/Stopień</label>
-        <input type="text" class="input" name="degree" placeholder="Stopień/tytuł" value="${data.degree || ''}" />
-		<label for="edu-start" class="label">Od:</label>
-        <input type="date"  class="input" name="edu-start" value="${data.start || ''}" />
-		<label for="edu-end" class="label">Do:</label>
-        <input type="date" class="input" name="edu-end" value="${data.end || ''}" />
-        <button class="delete">Usuń</button>
-    `
-	form.querySelectorAll('input').forEach(input => {
-		input.addEventListener('input', saveEduData)
-	})
-	form.querySelector('.delete').addEventListener('click', () => {
-		form.remove()
-		saveEduData()
-	})
-	eduContainer.appendChild(form)
-}
-const createLanguage = (data = {}) => {
-	const langContainer = document.querySelector('.cv-box-lang')
-	const form = document.createElement('form')
-	form.classList.add('language-form', 'form')
-	form.setAttribute('data-id', data.id || Date.now().toString())
-	form.innerHTML = `
-		<label for="language" class="label">Język:</label>
-    	<input type="text"  class="input" id="language" name="language" placeholder="Angielski" value="${
-				data.language || ''
-			}">
-    	<label for="level" class="label"></label>
-      	<input type="text" class="input" id="level" name="level" placeholder="C1" value="${data.level || ''}">
-     	<button class="delete">Usuń</button>
-	`
-	form.querySelectorAll('input').forEach(input => {
-		input.addEventListener('input', saveLangData)
-	})
-	form.querySelector('.delete').addEventListener('click', () => {
-		form.remove()
-		saveLangData()
-	})
-	langContainer.appendChild(form)
-}
-const createSkills = (data = {}) => {
-	const skillsContainer = document.querySelector('.cv-box-skills')
-	const form = document.createElement('form')
-	form.classList.add('skills-form', 'form')
-	form.setAttribute('data-id', data.id || Date.now().toString())
-	form.innerHTML = ` 
-	<label class="label" for="skills">Umiętność:</label>
-    <input class="input" type="text" id="skills" name="skills" value="${
-			data.skill || ''
-		}" placeholder="Znajmość Google Search Console">
-	<button class="delete">Usuń</button>
-	`
-	form.querySelectorAll('input').forEach(input => {
-		input.addEventListener('input', saveSkillsData)
-	})
-	form.querySelector('.delete').addEventListener('click', () => {
-		form.remove()
-		saveSkillsData()
-	})
-	skillsContainer.append(form)
-}
-const generateUserDataAbout = () => {
-	const userName = document.querySelector('.name-preview')
-	const userEmail = document.querySelector('.email-preview')
-	const userTel = document.querySelector('.tel-preview')
-
-	const formName = document.querySelector('#name')
-	const formLastName = document.querySelector('#last-name')
-	const formEmail = document.querySelector('#email')
-	const formTel = document.querySelector('#tel')
-
-	if (userName && formName && formLastName) {
-		userName.textContent = `${formName.value} ${formLastName.value}`.trim()
-	}
-	if (userEmail && formEmail) {
-		userEmail.textContent = formEmail.value
-	}
-	if (userTel && formTel) {
-		userTel.textContent = formTel.value
-	}
-}
 const generateUserJob = () => {
 	const previewJob = document.querySelector('.cv-preview-job')
 	previewJob.innerHTML = '<h2 class="cv-preview-job-title">Doświadczenie:</h2>'
@@ -158,25 +68,51 @@ const generateUserJob = () => {
 		previewJob.append(jobBox)
 	})
 }
-const generateUserLang = () => {
-	const previewLang = document.querySelector('.cv-preview-lang')
-	previewLang.innerHTML = '<h2 class="cv-preview-title">Języki:</h2>'
-	const savedData = JSON.parse(localStorage.getItem('langData')) || []
-	savedData.forEach(data => {
-		const langBox = document.createElement('div')
-		langBox.classList.add('preview-lang-box')
-		const lang = document.createElement('p')
-		lang.classList.add('preview-lang-lang')
-		lang.textContent = data.language
-		const langSpace = document.createElement('p')
-		langSpace.classList.add('space')
-		langSpace.textContent = '-'
-		const langLvl = document.createElement('p')
-		langLvl.classList.add('preview-lang-lvl')
-		langLvl.textContent = data.level
-		langBox.append(lang, langSpace, langLvl)
-		previewLang.append(langBox)
+const saveJobData = () => {
+	const jobForms = document.querySelectorAll('.job-form')
+	let jobData = []
+	jobForms.forEach(form => {
+		const id = form.getAttribute('data-id') || Date.now().toString()
+		const jobName = form.querySelector('[name="company"]').value
+		const jobRole = form.querySelector('[name="job-title"]').value
+		const jobStart = form.querySelector('[name="job-start"]').value
+		const jobEnd = form.querySelector('[name="job-end"]').value
+		const jobDescription = form.querySelector('[name="job-description"]').value
+		jobData.push({ id, company: jobName, role: jobRole, start: jobStart, end: jobEnd, description: jobDescription })
 	})
+	localStorage.setItem('jobData', JSON.stringify(jobData))
+}
+const loadJobData = () => {
+	const savedData = JSON.parse(localStorage.getItem('jobData')) || []
+	savedData.forEach(data => {
+		createJob(data)
+	})
+	generateUserJob()
+}
+const createEduForm = (data = {}) => {
+	const eduContainer = document.querySelector('.cv-box-edu') // Kontener na formularze
+	const form = document.createElement('form')
+	form.classList.add('edu-form', 'form')
+	form.setAttribute('data-id', data.id || Date.now().toString())
+	form.innerHTML = `
+		<label for="school" class="label">Szkoła/Uczelnia </label>
+        <input type="text" class="input" name="school" placeholder="Nazwa szkoły" value="${data.school || ''}" />
+		<label for="degree" class="label">Kierunek/Stopień</label>
+        <input type="text" class="input" name="degree" placeholder="Stopień/tytuł" value="${data.degree || ''}" />
+		<label for="edu-start" class="label">Od:</label>
+        <input type="date"  class="input" name="edu-start" value="${data.start || ''}" />
+		<label for="edu-end" class="label">Do:</label>
+        <input type="date" class="input" name="edu-end" value="${data.end || ''}" />
+        <button class="delete">Usuń</button>
+    `
+	form.querySelectorAll('input').forEach(input => {
+		input.addEventListener('input', saveEduData)
+	})
+	form.querySelector('.delete').addEventListener('click', () => {
+		form.remove()
+		saveEduData()
+	})
+	eduContainer.appendChild(form)
 }
 const generateUserEdu = () => {
 	const previewEdu = document.querySelector('.cv-preview-edu')
@@ -207,6 +143,111 @@ const generateUserEdu = () => {
 		previewEdu.append(eduBox)
 	})
 }
+const saveEduData = () => {
+	const eduForms = document.querySelectorAll('.edu-form')
+	let eduData = []
+	eduForms.forEach(form => {
+		const id = form.getAttribute('data-id') || Date.now().toString()
+		form.setAttribute('data-id', id)
+		const eduSchool = form.querySelector('[name="school"]').value
+		const eduRoleLvl = form.querySelector('[name="degree"]').value
+		const eduStart = form.querySelector('[name="edu-start"]').value
+		const eduEnd = form.querySelector('[name="edu-end"]').value
+		eduData.push({ id, school: eduSchool, degree: eduRoleLvl, start: eduStart, end: eduEnd })
+	})
+	localStorage.setItem('eduData', JSON.stringify(eduData))
+}
+const loadEduData = () => {
+	const savedData = JSON.parse(localStorage.getItem('eduData')) || []
+	savedData.forEach(data => {
+		createEduForm(data)
+	})
+	generateUserEdu()
+}
+const createLanguage = (data = {}) => {
+	const langContainer = document.querySelector('.cv-box-lang')
+	const form = document.createElement('form')
+	form.classList.add('language-form', 'form')
+	form.setAttribute('data-id', data.id || Date.now().toString())
+	form.innerHTML = `
+		<label for="language" class="label">Język:</label>
+    	<input type="text"  class="input" id="language" name="language" placeholder="Angielski" value="${
+				data.language || ''
+			}">
+    	<label for="level" class="label"></label>
+      	<input type="text" class="input" id="level" name="level" placeholder="C1" value="${data.level || ''}">
+     	<button class="delete">Usuń</button>
+	`
+	form.querySelectorAll('input').forEach(input => {
+		input.addEventListener('input', saveLangData)
+	})
+	form.querySelector('.delete').addEventListener('click', () => {
+		form.remove()
+		saveLangData()
+	})
+	langContainer.appendChild(form)
+}
+const generateUserLang = () => {
+	const previewLang = document.querySelector('.cv-preview-lang')
+	previewLang.innerHTML = '<h2 class="cv-preview-title">Języki:</h2>'
+	const savedData = JSON.parse(localStorage.getItem('langData')) || []
+	savedData.forEach(data => {
+		const langBox = document.createElement('div')
+		langBox.classList.add('preview-lang-box')
+		const lang = document.createElement('p')
+		lang.classList.add('preview-lang-lang')
+		lang.textContent = data.language
+		const langSpace = document.createElement('p')
+		langSpace.classList.add('space')
+		langSpace.textContent = '-'
+		const langLvl = document.createElement('p')
+		langLvl.classList.add('preview-lang-lvl')
+		langLvl.textContent = data.level
+		langBox.append(lang, langSpace, langLvl)
+		previewLang.append(langBox)
+	})
+}
+const saveLangData = () => {
+	const langForms = document.querySelectorAll('.language-form')
+	let langData = []
+	langForms.forEach(form => {
+		const id = form.getAttribute('data-id') || Date.now().toString()
+		form.setAttribute('data-id', id)
+		const langName = form.querySelector('[name="language"]').value
+		const langLevel = form.querySelector('[name="level"]').value
+		langData.push({ id, language: langName, level: langLevel })
+	})
+	localStorage.setItem('langData', JSON.stringify(langData))
+}
+const loadLangData = () => {
+	const savedData = JSON.parse(localStorage.getItem('langData')) || []
+	savedData.forEach(data => {
+		createLanguage(data)
+	})
+	generateUserLang() 
+}
+
+const createSkills = (data = {}) => {
+	const skillsContainer = document.querySelector('.cv-box-skills')
+	const form = document.createElement('form')
+	form.classList.add('skills-form', 'form')
+	form.setAttribute('data-id', data.id || Date.now().toString())
+	form.innerHTML = ` 
+	<label class="label" for="skills">Umiętność:</label>
+    <input class="input" type="text" id="skills" name="skills" value="${
+			data.skill || ''
+		}" placeholder="Znajmość Google Search Console">
+	<button class="delete">Usuń</button>
+	`
+	form.querySelectorAll('input').forEach(input => {
+		input.addEventListener('input', saveSkillsData)
+	})
+	form.querySelector('.delete').addEventListener('click', () => {
+		form.remove()
+		saveSkillsData()
+	})
+	skillsContainer.append(form)
+}
 const generateUserSkills = () => {
 	const previewSkills = document.querySelector('.cv-preview-skills')
 	previewSkills.innerHTML = ' <h2 class="cv-preview-skills-title">Umiejętności:</h2>'
@@ -223,6 +264,62 @@ const generateUserSkills = () => {
 		previewSkills.appendChild(skillList)
 	}
 }
+const saveSkillsData = () => {
+	const skillsForms = document.querySelectorAll('.skills-form')
+	let skillsData = []
+	skillsForms.forEach(form => {
+		const id = form.getAttribute('data-id') || Date.now().toString()
+		const skill = form.querySelector('[name="skills"]').value
+		skillsData.push({ id, skill: skill })
+	})
+	localStorage.setItem('skillsData', JSON.stringify(skillsData))
+}
+const loadSkillData = () => {
+	const savedData = JSON.parse(localStorage.getItem('skillsData')) || []
+	savedData.forEach(data => {
+		createSkills(data)
+	})
+	generateUserSkills()
+}
+
+const generateUserDataAbout = () => {
+	const userName = document.querySelector('.name-preview')
+	const userEmail = document.querySelector('.email-preview')
+	const userTel = document.querySelector('.tel-preview')
+	const formName = document.querySelector('#name')
+	const formLastName = document.querySelector('#last-name')
+	const formEmail = document.querySelector('#email')
+	const formTel = document.querySelector('#tel')
+
+	if (userName && formName && formLastName) {
+		userName.textContent = `${formName.value} ${formLastName.value}`.trim()
+	}
+	if (userEmail && formEmail) {
+		userEmail.textContent = formEmail.value
+	}
+	if (userTel && formTel) {
+		userTel.textContent = formTel.value
+	}
+}
+const saveFormsDataAbout = () => {
+	const inputs = document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]')
+	inputs.forEach(input => {
+		localStorage.setItem(input.name, input.value)
+	})
+	generateUserDataAbout()
+}
+const loadFormsUserAbout = () => {
+	const inputs = document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]')
+	inputs.forEach(input => {
+		input.value = localStorage.getItem(input.name) || ''
+	})
+	generateUserDataAbout()
+}
+document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]').forEach(input => {
+	input.addEventListener('input', saveFormsDataAbout)
+})
+
+
 const generatePreview = () => {
 	generateUserDataAbout()
 	generateUserJob()
@@ -265,101 +362,6 @@ const clearAll = () => {
 	localStorage.clear()
 }
 //LocalStorage
-const saveFormsDataAbout = () => {
-	const inputs = document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]')
-	inputs.forEach(input => {
-		localStorage.setItem(input.name, input.value)
-	})
-	generateUserDataAbout()
-}
-const loadFormsUserAbout = () => {
-	const inputs = document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]')
-	inputs.forEach(input => {
-		input.value = localStorage.getItem(input.name) || ''
-	})
-	generateUserDataAbout()
-}
-document.querySelectorAll('[name="name"], [name="last-name"], [name="email"], [name="tel"]').forEach(input => {
-	input.addEventListener('input', saveFormsDataAbout)
-})
-const saveEduData = () => {
-	const eduForms = document.querySelectorAll('.edu-form')
-	let eduData = []
-	eduForms.forEach(form => {
-		const id = form.getAttribute('data-id') || Date.now().toString()
-		form.setAttribute('data-id', id)
-		const eduSchool = form.querySelector('[name="school"]').value
-		const eduRoleLvl = form.querySelector('[name="degree"]').value
-		const eduStart = form.querySelector('[name="edu-start"]').value
-		const eduEnd = form.querySelector('[name="edu-end"]').value
-		eduData.push({ id, school: eduSchool, degree: eduRoleLvl, start: eduStart, end: eduEnd })
-	})
-	localStorage.setItem('eduData', JSON.stringify(eduData))
-}
-const loadEduData = () => {
-	const savedData = JSON.parse(localStorage.getItem('eduData')) || []
-	savedData.forEach(data => {
-		createEduForm(data)
-	})
-	generateUserEdu()
-}
-const saveLangData = () => {
-	const langForms = document.querySelectorAll('.language-form')
-	let langData = []
-	langForms.forEach(form => {
-		const id = form.getAttribute('data-id') || Date.now().toString()
-		form.setAttribute('data-id', id)
-		const langName = form.querySelector('[name="language"]').value
-		const langLevel = form.querySelector('[name="level"]').value
-		langData.push({ id, language: langName, level: langLevel })
-	})
-	localStorage.setItem('langData', JSON.stringify(langData))
-}
-const loadLangData = () => {
-	const savedData = JSON.parse(localStorage.getItem('langData')) || []
-	savedData.forEach(data => {
-		createLanguage(data)
-	})
-	generateUserLang() // Po załadowaniu formularzy aktualizujemy podgląd CV
-}
-const saveJobData = () => {
-	const jobForms = document.querySelectorAll('.job-form')
-	let jobData = []
-	jobForms.forEach(form => {
-		const id = form.getAttribute('data-id') || Date.now().toString()
-		const jobName = form.querySelector('[name="company"]').value
-		const jobRole = form.querySelector('[name="job-title"]').value
-		const jobStart = form.querySelector('[name="job-start"]').value
-		const jobEnd = form.querySelector('[name="job-end"]').value
-		const jobDescription = form.querySelector('[name="job-description"]').value
-		jobData.push({ id, company: jobName, role: jobRole, start: jobStart, end: jobEnd, description: jobDescription })
-	})
-	localStorage.setItem('jobData', JSON.stringify(jobData))
-}
-const loadJobData = () => {
-	const savedData = JSON.parse(localStorage.getItem('jobData')) || []
-	savedData.forEach(data => {
-		createJob(data)
-	})
-	generateUserJob()
-}
-const saveSkillsData = () => {
-	const skillsForms = document.querySelectorAll('.skills-form')
-	let skillsData = []
-	skillsForms.forEach(form => {
-		const id = form.getAttribute('data-id') || Date.now().toString()
-		const skill = form.querySelector('[name="skills"]').value
-		skillsData.push({ id, skill: skill })
-	})
-	localStorage.setItem('skillsData', JSON.stringify(skillsData))
-}
-const loadSkillData = () => {
-	const savedData = JSON.parse(localStorage.getItem('skillsData')) || []
-	savedData.forEach(data => {
-		createSkills(data)
-	})
-	generateUserSkills()
-}
 const loadLocalStorage = () => {
 	loadFormsUserAbout()
 	loadEduData()
@@ -377,7 +379,6 @@ const downloadPdf = () => {
 		jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' },
 	}
 	html2pdf().set(options).from(element).save()
-
 }
 
 jobBtn.addEventListener('click', createJob)
